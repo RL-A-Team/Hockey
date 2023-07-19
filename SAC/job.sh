c804b5ba536a8aed22a2309a9b08554561f136b8
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-07-19 18:27:41 +0200

    changed job

--- SAC/job.sh
+++ SAC/job.sh
@@ -2,6 +2,8 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=1         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
+#SBATCH --time=0-01:00            # Runtime in D-HH:MM
+#SBATCH --mem=10G                # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=logs/job_%j.out  # File to which STDOUT will be written - make sure this is not on $HOME
 #SBATCH --error=logs/job_%j.err   # File to which STDERR will be written - make sure this is not on $HOME
 #SBATCH --mail-type=ALL           # Type of email notification- BEGIN,END,FAIL,ALL
