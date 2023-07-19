2f1f10eb9af759dec9c5542f407fecf99186e7c2
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-07-19 18:26:32 +0200

    changed job

--- SAC/job.sh
+++ SAC/job.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=1         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-3:00            # Runtime in D-HH:MM
+#SBATCH --time=0-03:00            # Runtime in D-HH:MM
 #SBATCH --gres=gpu:1    # optionally type and number of gpus
 #SBATCH --mem=10G                # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=logs/job_%j.out  # File to which STDOUT will be written - make sure this is not on $HOME
