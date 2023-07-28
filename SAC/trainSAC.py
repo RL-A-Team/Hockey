81d0b6647bfc6bd8e9ff08830c2b0da58e9d2c59
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-07-28 19:32:25 +0200

    changed reward

--- SAC/trainSAC.py
+++ SAC/trainSAC.py
@@ -45,6 +45,7 @@ if __name__ == '__main__':
             ns, r, d, _, info = env.step(np.hstack([a1, a2]))
 
             reward = r + info['reward_closeness_to_puck'] + info['reward_puck_direction']
+            print('Reward', reward)
 
             total_reward += reward
 
