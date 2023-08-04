853af585e06e0455cb34144ac7b29681df4c5adf
Frau-Ke <67635226+Frau-Ke@users.noreply.github.com>
2023-08-04 23:48:30 +0200

    [DDPG] Create ReplayBuffer.py

---
+++ DDPG/ReplayBuffer.py
@@ -0,0 +1,21 @@
+class ReplayBuffer:
+        def __init__(self, buffer_size):
+            self.buffer_size = buffer_size
+            self.buffer = []
+
+        def add(self, state, action, next_state, reward, done):
+            experience = (state, action, next_state, reward, done)
+            if len(self.buffer) < self.buffer_size:
+                self.buffer.append(experience)
+            else:
+                self.buffer.pop(0)
+                self.buffer.append(experience)
+
+        def get_size(self):
+            return len(self.buffer)
+
+        def sample(self, batch_size):
+            indices = np.random.choice(len(self.buffer), batch_size, replace=False)
+            batch = [self.buffer[idx] for idx in indices]
+            states, actions, next_states, rewards, dones = zip(*batch)
+            return states, actions, next_states, rewards, dones
