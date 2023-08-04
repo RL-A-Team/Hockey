6c50755bb47c84e70f9e4752efc7436526d708b9
Indira <indira.echsler@gmx.net>
2023-08-04 18:23:40 +0200

    edit job.sh

--- DDDQN/job.sh
+++ DDDQN/job.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem-per-cpu=1G
 #SBATCH --gres=gpu:1    	  # optionally type and number of gpus
 #SBATCH --time=00:15:00           # Runtime in D-HH:MM
-#SBATCH --output=logs/job_%j.out  # File to which STDOUT will be written - make sure this is not on $HOME
-#SBATCH --error=logs/job_%j.err   # File to which STDERR will be written - make sure this is not on $HOME
+#SBATCH --output=indiras_logs/outfiles/job_%j.out  # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=indiras_logs/errors/job_%j.err   # File to which STDERR will be written - make sure this is not on $HOME
 #SBATCH --mail-type=ALL           # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=indira.echsler@student.uni-tuebingen.de   # Email to which notifications will be sent
 
