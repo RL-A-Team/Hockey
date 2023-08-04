7a0963ffa228be449ff13be29deaaf6470c097bd
Indira <indira.echsler@gmx.net>
2023-08-04 18:00:14 +0200

    debug

--- DDDQN/train_factors.py
+++ DDDQN/train_factors.py
@@ -11,17 +11,19 @@ weak_opponent = True
 #game_mode = h_env.HockeyEnv_BasicOpponent.TRAIN_DEFENSE
 #game_mode = h_env.HockeyEnv_BasicOpponent.TRAIN_SHOOTING
 game_mode = h_env.HockeyEnv_BasicOpponent.NORMAL
-episodes = 250
+episodes = 2# 250
 use_checkpoint = False
 visualize = False
 
 subtasks = 20
 
 current_subtask = 0
-try:
-    current_subtask = int(sys.argv[1])
-except ValueError:
-    print('current subtask [integer] required')
+#try:
+#    current_subtask = int(sys.argv[1])
+#except ValueError:
+#    print('current subtask [integer] required')
+
+print(sys.argv)
 
 factors = []
 
