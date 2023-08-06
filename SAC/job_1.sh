2d7bb3a02799f332d82af4f6a5278ba797a6b5fc
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-08-06 13:39:56 +0200

    next round

--- SAC/job_1.sh
+++ SAC/job_1.sh
@@ -15,5 +15,5 @@ scontrol show job $SLURM_JOB_ID
 source $HOME/.bashrc
 
 # insert your commands here
-singularity exec --nv /home/stud54/Hockey/tcml_singularity_rl_lecture/rl_lecture python3 /home/stud54/Hockey/SAC/trainSAC.py --episodes 1500 --autotune --loss l2 --gradientsteps 16 --lr 1e-4
+singularity exec --nv /home/stud54/Hockey/tcml_singularity_rl_lecture/rl_lecture python3 /home/stud54/Hockey/SAC/trainSAC.py --episodes 1500 --autotune --loss l2 --gradientsteps 16 --lr 1e-4 --model /home/stud54/Hockey/models/sac_model_20230806T132846.pkl
 
