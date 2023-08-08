dca8786e61da3a40e83d9aa56ff456be90a78024
Frau-Ke <67635226+Frau-Ke@users.noreply.github.com>
2023-08-08 17:24:42 +0200

    Add files via upload

--- DDPG/Noice.py
+++ DDPG/Noice.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import numpy as np
 
 class OUNoise():
@@ -18,4 +19,26 @@ class OUNoise():
         return noise
 
     def reset(self) -> None:
+=======
+import numpy as np
+
+class OUNoise():
+    def __init__(self, shape, theta: float = 0.15, dt: float = 1e-2):
+        self._shape = shape
+        self._theta = theta
+        self._dt = dt
+        self.noise_prev = np.zeros(self._shape)
+        self.reset()
+
+    def __call__(self) -> np.ndarray:
+        noise = (
+            self.noise_prev
+            + self._theta * ( - self.noise_prev) * self._dt
+            + np.sqrt(self._dt) * np.random.normal(size=self._shape)
+        )
+        self.noise_prev = noise
+        return noise
+
+    def reset(self) -> None:
+>>>>>>> 2fe89b120e8ed3f5140ed9858fa1143bc77a9ce3
         self.noise_prev = np.zeros(self._shape)
